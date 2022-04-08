3bcf4ff9631995e74c901dac8f8f532e145157d5
Rikard Vinge <rikard.vinge@volvo.com>
2022-04-08 16:19:28 +0200

    Modify setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     license='BSD 3',
     packages=find_packages(),
     install_requires=['numpy>=1.21.5', 'scikit-learn>=1.0.2'],
-    extras_requires={'dev': ['pytest>=6.2.5', 'matplotlib']},
+    extras_require={'dev': ['pytest>=6.2.5', 'matplotlib']},
     keywords=['feature importance', 'support vector machine', 'svm', 'feature selection', 'polynomial kernel'],
     classifiers= [
         'Development Status :: 4 - Beta',
