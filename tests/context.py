b74bb7cf67189331642684dcc58c45ea8f643efb
Rikard Vinge <rik.vinge@gmail.com>
2023-02-15 07:37:50 +0100

    Fix relative import paths in test suite

--- tests/context.py
+++ tests/context.py
@@ -1,5 +1,5 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'explainpolysvm')))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'explainpolysvm')))
 
-import expsvm
+import explainpolysvm.expsvm as expsvm
